884eb46e5ba2167619af89f8323d3d6664a154b6
Rutsuo <r.vanrees@icloud.com>
2025-07-08 13:13:39 +0200

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Analytics } from "@vercel/analytics/next";
 import { Geist } from "next/font/google";
 import Head from "next/head";
+import Script from "next/script";
 import "./globals.css";
 
 const font = Geist({
@@ -22,6 +23,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Head>
+        {/* Google AdSense script */}
+        <Script
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3493162279498337"
+          strategy="afterInteractive"
+          crossOrigin="anonymous"
+        />
         <meta name="google-adsense-account" content="ca-pub-3493162279498337" />
       </Head>
       <body className={`${font.className} antialiased`}>{children}</body>
